0fb194c6ec00a12e1a52100536f57e842059d3c9
Jingyi Gao <faygao52@gmail.com>
2021-06-23 19:16:22 +1000

    finish the test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
-
+const OK_STATUS = 200
+const SUCCESS_KEY = 'Arnie Quote'
+const FAILURE_KEY = 'FAILURE'
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const requests = urls.map(httpGet)
+  let responses = await Promise.all(requests)
+  return responses.map(_formatResult)
 };
+
+/**
+ * Format the response to 
+ * {key: response body }
+ * 
+ * @param {Object} response 
+ * @returns {Object}
+ */
+const _formatResult = (response) => {
+  const body = JSON.parse(response.body)
+  const message = body.message || ''
+  if (response.status === OK_STATUS) {
+    return { [SUCCESS_KEY]: message }
+  }
+  return { [FAILURE_KEY]: message }
+}
 
 module.exports = {
   getArnieQuotes,
